53eae29cb40e662113b877a5b657f57e0362d756
whyubullieme <ximingc1219@gmail.com>
2025-04-09 18:02:07 -0400

    Partial master view

---
+++ src/app/components/product-details/product-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details',
+  standalone: false,
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.css'
+})
+export class ProductDetailsComponent {
+
+}
